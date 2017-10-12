98f91bce57b426734817f71245fe498244a0e33d
Josh Parkinson <JParkinson1991@2957479.no-reply.drupal.org>
2017-10-12 21:43:04 +0100

    pareview code tweaks/formatting

--- hook_rebuild.api.php
+++ hook_rebuild.api.php
@@ -1,27 +1,26 @@
 <?php
+
 /**
  * @file
  * Module API File.
- * Module: hook_rebuild
+ * Module: hook_rebuild.
  *
  * Outlines usages of hooks created by this module.
- *
- * @author Josh Parkinson
  */
 
 /**
  * Rebuild data based upon refreshed caches.
  *
  * This hook allows your module to rebuild its data based on the latest/current
- * module data. It runs after hook_flush_caches() so it can be assumed all module
- * data fetched here is fresh/current.
+ * module data. It runs after hook_flush_caches() so it can be assumed all
+ * module data fetched here is fresh/current.
  *
  * IMPORTANT: Do not call exit() within this hook. It will stop execution of
- * all subsequent rebuild hooks and will annoy alot of people.
+ * all subsequent rebuild hooks and will annoy a lot of people.
  */
-function hook_rebuild(){
-    //Rebuild all cache heavy/dependent module functionality here.
-    //Note: It is very likely that any output created during this hook will not
-    //be displayed on screen. To check the hook is being invoked make use of the
-    //watchdog() log functionality.
+function hook_rebuild() {
+  // Rebuild all cache heavy/dependent module functionality here.
+  // Note: It is very likely that any output created during this hook will not
+  // be displayed on screen. To check the hook is being invoked make use of the
+  // watchdog() log functionality.
 }
